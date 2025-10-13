2cde15ae4871cd1a97cedc485b8cdfed7b93080b
hannahlin07 <cherry92111081128@gmail.com>
2025-10-14 02:17:28 +0800

    個人網站網址

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@
               <li style="font-size:20px;">📧 Email: cherry92111081128@gmail.com</li>
               <li style="font-size:20px;">📱 Phone: 0912577792</li>
               <li style="font-size:20px;">💻 GitHub: 
-                <a href="https://hannahlin07.github.io/hannahweb2025/" target="_blank">HannahWeb</a>
+                <a href="https://hannahlin07.github.io/hannahweb2025/" target="_blank">Hannah's Web</a>
               </li>
             </ul>
           </div>
